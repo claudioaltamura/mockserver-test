904caca9ec77c2628d374eabf6c72cb98d9a2bfc
claudioaltamura <claudio@claudioaltamura.de>
2026-03-31 10:40:16 +0200

    fix: test
    why?
    application context couldn't be loaded

--- src/main/java/de/claudioaltamura/mockserver/test/Config.java
+++ src/main/java/de/claudioaltamura/mockserver/test/Config.java
@@ -6,6 +6,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class Config {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
         return builder.build();
